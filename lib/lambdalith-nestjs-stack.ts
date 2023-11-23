8a4a4d0f06ccaf3b4fd6796d22ac2fa4704a5831
Carlos Rojas <crojas@myxsales.com>
2023-11-23 16:34:12 -0400

    base infra

--- lib/lambdalith-nestjs-stack.ts
+++ lib/lambdalith-nestjs-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class LambdalithNestjsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +14,43 @@ export class LambdalithNestjsStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'LambdalithNestjsQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // LambdaNestStack in stack.ts
+    const apiNestHandlerFunction = new lambda.Function(this, "ApiNestHandler", {
+      code: Code.fromAsset("api/dist"), // 👈 This is crucial
+      runtime: Runtime.NODEJS_18_X,
+      handler: "main.handler",
+      environment: {}, // 👈 You might need env variables
+    });
+
+    //An API REST
+    const api = new cdk.aws_apigateway.RestApi(this, "Api", {
+
+      deploy: true,
+      defaultMethodOptions: {
+        apiKeyRequired: true
+      }
+    });
+
+
+    api.root.addProxy({
+      defaultIntegration: new cdk.aws_apigateway.LambdaIntegration(apiNestHandlerFunction),
+    })
+
+    const apiKey = api.addApiKey("ApiKey");
+
+    const usagePlan = api.addUsagePlan("UsagePlan", {
+      name: "UsagePlan",
+      apiStages: [
+        {
+          api,
+          stage: api.deploymentStage
+        }
+      ]
+    }
+    );
+
+    usagePlan.addApiKey(apiKey);
+
   }
 }
